a12fce055f6a438b82a4217c3a2083391fd203a2
colby <colby88cardell@gmail.com>
2021-02-19 17:50:32 -0500

    :sparkles: Add countdown timer

--- src/components/Countdown/index.js
+++ src/components/Countdown/index.js
@@ -1,3 +1,20 @@
-const Component = () => <p className="">Countdown!</p>;
+import { useEffect, useState } from "react";
+
+const Component = () => {
+  const [timeRemaining, setTimeRemaining] = useState(10);
+
+  // It is a reference to any side effects - such as re-rendering.
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTimeRemaining(() => timeRemaining - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  });
+
+  return <p>{timeRemaining} seconds</p>;
+};
 
 export default Component;
