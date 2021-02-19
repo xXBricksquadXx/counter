72880298b443093c74d8e30c40e4ebbca808e86c
colby <colby88cardell@gmail.com>
2021-02-19 18:58:09 -0500

    :sparkles: Add todos when clicking button

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,12 +1,21 @@
 import { useState } from "react";
 
+function renderTodos(todos) {
+  return (
+    <ol>
+      {todos.map((todo, index) => (
+        <li key={index}>{todo}</li>
+      ))}
+    </ol>
+  );
+}
+
 const Todo = () => {
   const [todoList, setTodoList] = useState([]);
 
   function handleSubmit(ev) {
     ev.preventDefault();
 
-    // TODO: Update state with whatever we typed
     setTodoList((prevTodoList) => [
       ...prevTodoList,
       ev.target.elements[0].value,
@@ -16,6 +25,10 @@ const Todo = () => {
   return (
     <main className="container mx-auto flex flex-col items-center">
       <h1 className="text-3xl font-bold uppercase">Todo</h1>
+
+      {/* Conditional rendering */}
+      {todoList.length ? renderTodos(todoList) : null}
+
       <form className="flex flex-col items-center mt-2" onSubmit={handleSubmit}>
         <div className="flex flex-col items-center mt-2">
           <label htmlFor="todo">What needs to be done?</label>
@@ -24,7 +37,7 @@ const Todo = () => {
 
         <button
           type="submit"
-          className="bg-indigo-500 text-white rounded mt-4 px-4 py-2"
+          className="bg-indigo-700 text-indigo-200 rounded mt-4 px-4 py-2"
         >
           Add #{todoList.length + 1}
         </button>
