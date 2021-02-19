f5398da7e751ab2791d4f671cedfd452eb2fc43f
colby <colby88cardell@gmail.com>
2021-02-19 18:52:08 -0500

    :construction: Submit handler logging

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -3,10 +3,16 @@ import { useState } from "react";
 const Todo = () => {
   const [todoList, setTodoList] = useState([]);
 
+  function handleSubmit(ev) {
+    ev.preventDefault();
+
+    console.log("submit");
+  }
+
   return (
     <main className="container mx-auto flex flex-col items-center">
       <h1 className="text-3xl font-bold uppercase">Todo</h1>
-      <form className="flex flex-col items-center mt-2">
+      <form className="flex flex-col items-center mt-2" onSubmit={handleSubmit}>
         <div className="flex flex-col items-center mt-2">
           <label htmlFor="todo">What needs to be done?</label>
           <input type="text" id="todo" />
@@ -14,7 +20,7 @@ const Todo = () => {
 
         <button
           type="submit"
-          className="bg-indigo-700 text-indigo-200 rounded mt-4 px-4 py-2"
+          className="bg-indigo-500 text-white rounded mt-4 px-4 py-2"
         >
           Add #{todoList.length + 1}
         </button>
